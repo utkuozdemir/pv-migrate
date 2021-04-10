297efc48f90cc24f86102b8380738cfdc4dbe22b
Utku Ozdemir <uoz@protonmail.com>
2021-04-10 21:54:29 +0200

    Improve test resources

--- test/test-run.sh
+++ test/test-run.sh
@@ -4,6 +4,27 @@ set -euo pipefail
 SOURCE_KUBECONFIG=.kubeconfig-source.yaml
 DEST_KUBECONFIG=.kubeconfig-dest.yaml
 
+# Same namespace
+pv-migrate \
+  --source-kubeconfig $SOURCE_KUBECONFIG \
+  --source-namespace source \
+  --source source \
+  --dest-kubeconfig $SOURCE_KUBECONFIG \
+  --dest-namespace source \
+  --dest dest \
+  --dest-delete-extraneous-files true
+
+# Different namespace
+pv-migrate \
+  --source-kubeconfig $SOURCE_KUBECONFIG \
+  --source-namespace source \
+  --source source \
+  --dest-kubeconfig $SOURCE_KUBECONFIG \
+  --dest-namespace dest \
+  --dest dest \
+  --dest-delete-extraneous-files true
+
+# Different cluster
 pv-migrate \
   --source-kubeconfig $SOURCE_KUBECONFIG \
   --source-namespace source \
