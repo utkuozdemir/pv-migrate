7e3a94fe18b531d404a119209448d2c623dad24f
Utku Ozdemir <uoz@protonmail.com>
2021-04-12 20:43:09 +0200

    Update test-run.sh for urfave/cli

--- test/test-run.sh
+++ test/test-run.sh
@@ -4,29 +4,41 @@ set -euo pipefail
 SOURCE_KUBECONFIG=.kubeconfig-source.yaml
 DEST_KUBECONFIG=.kubeconfig-dest.yaml
 
-# Same namespace
+echo "Same namespace"
 pv-migrate migrate \
   --source-kubeconfig $SOURCE_KUBECONFIG \
   --source-namespace source \
   --dest-kubeconfig $SOURCE_KUBECONFIG \
   --dest-namespace source \
-  --dest-delete-extraneous-files true \
+  --dest-delete-extraneous-files \
   source dest
 
-# Different namespace
+echo "Different namespace"
 pv-migrate migrate \
   --source-kubeconfig $SOURCE_KUBECONFIG \
   --source-namespace source \
   --dest-kubeconfig $SOURCE_KUBECONFIG \
   --dest-namespace dest \
-  --dest-delete-extraneous-files true \
+  --dest-delete-extraneous-files \
   source dest
 
-# Different cluster
-pv-migrate \
+echo "Different cluster"
+pv-migrate migrate \
   --source-kubeconfig $SOURCE_KUBECONFIG \
-  --source-namespace source \
+  -n source \
   --dest-kubeconfig $DEST_KUBECONFIG \
-  --dest-namespace dest \
-  --dest-delete-extraneous-files true \
+  -N dest \
+  --dest-delete-extraneous-files \
+  source dest
+
+kubectl --kubeconfig $SOURCE_KUBECONFIG config set-context --current --namespace=source
+kubectl --kubeconfig $DEST_KUBECONFIG config set-context --current --namespace=dest
+
+echo "Different cluster - implicit ns"
+pv-migrate m \
+  -k $SOURCE_KUBECONFIG \
+  -n source \
+  -K $DEST_KUBECONFIG \
+  -N dest \
+  -d \
   source dest
