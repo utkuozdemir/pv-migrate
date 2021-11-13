79f1a047e5383b5725504957fa83562310baf6bb
Utku Ozdemir <uoz@protonmail.com>
2021-11-13 22:20:09 +0100

    feat: implement svc strategy using helm
    Signed-off-by: Utku Ozdemir <uoz@protonmail.com>

--- internal/strategy/svc.go
+++ internal/strategy/svc.go
@@ -1,9 +1,11 @@
 package strategy
 
 import (
+	"github.com/utkuozdemir/pv-migrate/internal/k8s"
 	"github.com/utkuozdemir/pv-migrate/internal/rsync"
 	"github.com/utkuozdemir/pv-migrate/internal/task"
-	corev1 "k8s.io/api/core/v1"
+	"helm.sh/helm/v3/pkg/action"
+	"time"
 )
 
 type Svc struct {
@@ -17,11 +19,74 @@ func (r *Svc) canDo(t *task.Task) bool {
 }
 
 func (r *Svc) Run(e *task.Execution) (bool, error) {
-	if !r.canDo(e.Task) {
+	t := e.Task
+	if !r.canDo(t) {
 		return false, nil
+	}
+
+	sourceNs := t.Migration.Source.Namespace
+	destNs := t.Migration.Dest.Namespace
+
+	s := e.Task.SourceInfo
+	d := e.Task.DestInfo
+
+	helmActionConfig, err := initHelmActionConfig(e.Logger, e.Task.DestInfo)
+	if err != nil {
+		return true, err
+	}
+
+	install := action.NewInstall(helmActionConfig)
+	install.Namespace = destNs
+	install.ReleaseName = e.HelmReleaseName
+	install.Wait = true
+	install.Timeout = 1 * time.Minute
+
+	t.Logger.Info(":key: Generating SSH key pair")
+	keyAlgorithm := t.Migration.Options.KeyAlgorithm
+	publicKey, privateKey, err := rsync.CreateSSHKeyPair(keyAlgorithm)
+	privateKeyMountPath := "/root/.ssh/id_" + keyAlgorithm
+	if err != nil {
+		return true, err
+	}
+
+	opts := t.Migration.Options
+	vals := map[string]interface{}{
+		"rsync": map[string]interface{}{
+			"enabled":               true,
+			"deleteExtraneousFiles": opts.DeleteExtraneousFiles,
+			"noChown":               opts.NoChown,
+			"privateKeyMount":       true,
+			"privateKey":            privateKey,
+			"privateKeyMountPath":   privateKeyMountPath,
+		},
+		"sshd": map[string]interface{}{
+			"enabled":   true,
+			"publicKey": publicKey,
+		},
+		"source": map[string]interface{}{
+			"namespace":        sourceNs,
+			"pvcName":          s.Claim.Name,
+			"pvcMountReadOnly": opts.SourceMountReadOnly,
+			"path":             t.Migration.Source.Path,
+		},
+		"dest": map[string]interface{}{
+			"namespace": destNs,
+			"pvcName":   d.Claim.Name,
+			"path":      t.Migration.Dest.Path,
+		},
+	}
+
+	_, err = install.Run(t.Chart, vals)
+	if err != nil {
+		return true, err
 	}
 
 	doneCh := registerCleanupHook(e)
 	defer cleanupAndReleaseHook(e, doneCh)
-	return true, rsync.RunRsyncJobOverSSH(e, corev1.ServiceTypeClusterIP)
+
+	showProgressBar := !opts.NoProgressBar
+	kubeClient := t.SourceInfo.ClusterClient.KubeClient
+	jobName := e.HelmReleaseName + "-rsync"
+	err = k8s.WaitUntilJobIsCompleted(e.Logger, kubeClient, destNs, jobName, showProgressBar)
+	return true, err
 }
