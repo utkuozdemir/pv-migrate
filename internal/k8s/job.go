3b3179aa49c7fa589b067b57a4482d7e73896fe4
Utku Ozdemir <uoz@protonmail.com>
2021-11-19 21:47:31 +0100

    feat: tail pod logs with retry instead of polling them
    Signed-off-by: Utku Ozdemir <uoz@protonmail.com>

--- internal/k8s/job.go
+++ internal/k8s/job.go
@@ -3,24 +3,24 @@ package k8s
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	applog "github.com/utkuozdemir/pv-migrate/internal/log"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
-	"sync"
 )
 
 func WaitForJobCompletion(logger *log.Entry, cli kubernetes.Interface,
-	ns string, name string, showProgressBar bool) error {
+	ns string, name string, progressBarRequested bool) error {
 	pod, err := waitForJobPod(cli, ns, name)
 	if err != nil {
 		return err
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
-	defer wg.Wait()
 	successCh := make(chan bool, 1)
 
-	go tryLogProgressFromRsyncLogs(&wg, cli, pod, successCh, showProgressBar, logger)
+	showProgressBar := progressBarRequested &&
+		logger.Context.Value(applog.FormatContextKey) == applog.FormatFancy
+	go handlePodLogs(cli, ns, pod.Name, successCh, showProgressBar, logger)
+
 	p, err := waitForPodTermination(cli, pod.Namespace, pod.Name)
 	if err != nil {
 		successCh <- false
