411929f5f9552c67092e0e8b443027975b464784
FL42 <46161216+fl42@users.noreply.github.com>
2023-07-16 22:40:37 +0200

    fix: lint

--- internal/k8s/service.go
+++ internal/k8s/service.go
@@ -64,7 +64,7 @@ func GetServiceAddress(
 					result = res.Status.LoadBalancer.Ingress[0].Hostname
 				} else {
 					result = res.Status.LoadBalancer.Ingress[0].IP
-				}		
+				}
 
 				return true, nil
 			}
