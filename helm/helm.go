dc317a819c598a851e09c43d307b1ca8a1a957d9
Utku Ozdemir <utkuozdemir@gmail.com>
2024-04-21 13:49:51 +0200

    feat: read helm chart directly from the embedded directory
    Signed-off-by: Utku Ozdemir <utkuozdemir@gmail.com>

---
+++ helm/helm.go
@@ -0,0 +1,73 @@
+package helm
+
+import (
+	"embed"
+	"fmt"
+	"io/fs"
+	"path/filepath"
+
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chart/loader"
+)
+
+// chartFS is the embedded Helm chart.
+//
+// Note: The prefix "all:" is important here, as otherwise the files starting with "." or "_" will be ignored.
+//
+// See: https://github.com/golang/go/issues/44393
+//
+//go:embed all:pv-migrate
+var chartFS embed.FS
+
+const rootDir = "pv-migrate"
+
+// LoadChart loads the embedded Helm chart.
+func LoadChart() (*chart.Chart, error) {
+	files, err := chartAsBufferedFiles()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get chart files: %w", err)
+	}
+
+	helmChart, err := loader.LoadFiles(files)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load chart: %w", err)
+	}
+
+	return helmChart, nil
+}
+
+func chartAsBufferedFiles() ([]*loader.BufferedFile, error) {
+	var files []*loader.BufferedFile
+
+	err := fs.WalkDir(chartFS, rootDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		}
+
+		data, err := chartFS.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to read file %q in chart: %w", path, err)
+		}
+
+		relativePath, err := filepath.Rel(rootDir, path)
+		if err != nil {
+			return fmt.Errorf("failed to relativize path %q: %w", path, err)
+		}
+
+		files = append(files, &loader.BufferedFile{
+			Name: relativePath,
+			Data: data,
+		})
+
+		return nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to walk chart directory: %w", err)
+	}
+
+	return files, nil
+}
